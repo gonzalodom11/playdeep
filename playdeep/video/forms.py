2fb2ac37081d209cccf0749300d6e559112a806f
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-03-16 16:21:11 +0100

    feat: pagination & auto-slug

--- playdeep/video/forms.py
+++ playdeep/video/forms.py
@@ -1,6 +1,7 @@
 
 from django import forms
 from .models import Video
+from django.utils.text import slugify
 
 
 class Video_form(forms.ModelForm):
@@ -8,3 +9,10 @@ class Video_form(forms.ModelForm):
     class Meta:
         model = Video
         fields = ("caption", "video")
+    
+    def save(self, commit=True):
+        instance = super().save(commit=False)  # Don't save yet
+        instance.slug = slugify(instance.caption)  # Generate slug from caption
+        if commit:
+            instance.save()  # Save only if commit=True
+        return instance
--- playdeep/video/views.py
+++ playdeep/video/views.py
@@ -2,11 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Video
 from .forms import Video_form
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 # Create your views here.
 
 
 def index(request):
-    videos = Video.objects.all()
+    videos_list = Video.objects.all()
+    # Pagination with 2 videos per page
+    paginator = Paginator(videos_list, 2)
+    page_number = request.GET.get('page', 1)
+    
+    try:
+        videos = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        videos = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results.
+        videos = paginator.page(paginator.num_pages)
+
+
+
     if request.method == 'POST':
         form = Video_form(request.POST, request.FILES)
         if form.is_valid():
