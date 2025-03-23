2f4c3dc85ab075f1c1b119c7538fc4bfdfa1bc8e
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-03-23 23:36:54 +0100

    fix: warnings from inference

--- playdeep/video/views.py
+++ playdeep/video/views.py
@@ -4,19 +4,24 @@ from django.shortcuts import get_object_or_404, render
 from .models import Video
 from .forms import Video_form
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from roboflow import Roboflow
 from decouple import config
-from inference import get_model
 import supervision as sv
 from SoccerNet.Downloader import SoccerNetDownloader
 import cv2
 from PIL import Image
 import io
+from inference import get_model
+import warnings
 
 
-
-# Create your views here.
 myDownloader = SoccerNetDownloader(LocalDirectory="./SoccernetData/")
 os.environ["ONNXRUNTIME_EXECUTION_PROVIDERS"] = "[CPUExecutionProvider]"
+
+# Suppress only 'ModelDependencyMissing' warnings
+warnings.filterwarnings("ignore", category=UserWarning, message="ModelDependencyMissing.*")
+warnings.filterwarnings("ignore", message="Specified provider '.*' is not in available provider names")
+
 
 def index(request):
     videos_list = Video.objects.all()
@@ -63,13 +68,6 @@ def video_detail(request, year, month, day, video):
         {'video': video}
     )
 
-""" 
-def object_detection(request):
-    ROBOFLOW_API_KEY = config('ROBOFLOW_API_KEY')
-    PLAYER_DETECTION_MODEL_ID = "football-players-detection-3zvbc/11"
-    PLAYER_DETECTION_MODEL = get_model(model_id=PLAYER_DETECTION_MODEL_ID, api_key=ROBOFLOW_API_KEY) 
-"""
-
 def object_detection(request, year, month, day, video):
 
     video = get_object_or_404(
@@ -82,8 +80,10 @@ def object_detection(request, year, month, day, video):
     
     try:
         ROBOFLOW_API_KEY = config('ROBOFLOW_API_KEY')
-        PLAYER_DETECTION_MODEL_ID = "football-players-detection-3zvbc/11"
-        PLAYER_DETECTION_MODEL = get_model(model_id=PLAYER_DETECTION_MODEL_ID, api_key=ROBOFLOW_API_KEY)
+        # Initialize Roboflow
+        rf = Roboflow(api_key=ROBOFLOW_API_KEY)
+        # Load the model
+        PLAYER_DETECTION_MODEL = rf.workspace().project("football-players-detection-3zvbc").version(11).model
     except Exception as e:
         return HttpResponse(f"Error: Environment key missing", status=500)
     
@@ -121,6 +121,7 @@ def object_detection(request, year, month, day, video):
         scene=annotated_frame,
         detections=detections,
         labels=labels)
+        
 
     # Convert to image and serve as a response
     image = Image.fromarray(cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB))
@@ -130,5 +131,58 @@ def object_detection(request, year, month, day, video):
     return HttpResponse(buffer.getvalue(), content_type="image/png")
 
 
+def object_detection_plot():
+    
+    try:
+        ROBOFLOW_API_KEY = config('ROBOFLOW_API_KEY')
+        PLAYER_DETECTION_MODEL_ID = "football-players-detection-3zvbc/11"
+        PLAYER_DETECTION_MODEL = get_model(model_id=PLAYER_DETECTION_MODEL_ID, api_key=ROBOFLOW_API_KEY)
+    except Exception as e:
+        return HttpResponse(f"Error: Environment key missing", status=500)
+    
+
+    video_url = config(r'VIDEO_EXAMPLE')
+    
+
+    box_annotator = sv.BoxAnnotator(
+    color=sv.ColorPalette.from_hex(['#FF8C00', '#00BFFF', '#FF1493', '#FFD700']),
+    thickness=2
+    )
+    label_annotator = sv.LabelAnnotator(
+        color=sv.ColorPalette.from_hex(['#FF8C00', '#00BFFF', '#FF1493', '#FFD700']),
+        text_color=sv.Color.from_hex('#000000')
+    )
+
+    frame_generator = sv.get_video_frames_generator(video_url)
+    frame = next(frame_generator)
+
+    result = PLAYER_DETECTION_MODEL.infer(frame, confidence=0.3)[0]
+    #We passed the detection results into a supervision.Detections object for easier handling
+    detections = sv.Detections.from_inference(result)
+
+    labels = [
+        f"{class_name} {confidence:.2f}"
+        for class_name, confidence
+        in zip(detections['class_name'], detections.confidence)
+    ]
+
+    annotated_frame = frame.copy()
+    annotated_frame = box_annotator.annotate(
+        scene=annotated_frame,
+        detections=detections)
+    annotated_frame = label_annotator.annotate(
+        scene=annotated_frame,
+        detections=detections,
+        labels=labels)
+        
+
+    # Convert to image and serve as a response
+    image = Image.fromarray(cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB))
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+
+    sv.plot_image(annotated_frame)
+
+object_detection_plot()
 
 
