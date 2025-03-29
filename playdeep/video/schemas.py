3831e02d0266ee875df5ada5d416d7c049fd80f0
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-03-29 23:32:10 +0100

    feat: schema, api and router

---
+++ playdeep/video/schemas.py
@@ -0,0 +1,17 @@
+from ninja import Schema
+from datetime import datetime
+
+class VideoSchema(Schema):
+    id: int
+    caption: str
+    video: str  # This will store the file URL as a string
+    publish: datetime
+    slug: str
+
+    class Config:
+        from_attributes = True  # Allows conversion from Django model instances
+
+
+class VideoCreateSchema(Schema):
+    video: str  # This will store the file URL as a string
+    caption: str
