b987317f8a345cef3be46c9677ff7a0e8f56b509
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-04-16 02:08:49 +0200

    merge: delete duplicated navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,11 @@
 import { Hero } from "@/components";
 
-import Navbar from "@/components/header/Navbar";
 import Features from '@/components/home/Features';
 
 // import Image from "next/image";
 
 export default function Home() {
   return (
-      <><Navbar /><Hero /><Features /></>  
+      <><Hero /><Features /></>  
   );
 }
